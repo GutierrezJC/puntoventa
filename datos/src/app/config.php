e70c6ca3bad2423bc6cac6fad3a417fb63b57356
GutierrezJC <gutierrezcarrillo0794@outlook.es>
2025-05-06 13:38:29 -0600

    Renombrado archivo antiguo_nombre.sql a nuevo_nombre.sql

--- datos/src/app/config.php
+++ datos/src/app/config.php
@@ -3,7 +3,7 @@ $container -> set('config_bd', function(){
     return(object)[
 
         "host"=> "db",
-        "db"=> "ventas",
+        "db"=> "taller",
         "user"=> "root",
         "passw"=> "12345",
         "charset"=>"utf8mb4",
--- producto.sql
+++ producto.sql
@@ -1,4 +1,4 @@
- use ventas;
+-- use ventas;
 
 -- Create table Articulo(
 --     id_articulo int primary key auto_increment,
@@ -412,6 +412,25 @@ INSERT INTO `venta_detalle` (`id`, `nro_boleta`, `codigo_producto`, `cantidad`, 
 --
 -- Indices de la tabla `categorias`
 --
+
+
+CREATE TABLE usuario (
+    id int NOT NULL,
+    id_usuario varchar(15) NOT NULL,
+    correo varchar(100) NOT NULL,
+    rol int NOT NULL,
+    passw varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb4_spanish_ci NOT NULL,
+    ultimoAcceso datetime DEFAULT null,
+    tkR varchar(255) NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb4_spanish_ci;
+
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `idx_Usuario` (id_usuario);
+
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  
 ALTER TABLE `categorias`
   ADD PRIMARY KEY (`id_categoria`);
 
